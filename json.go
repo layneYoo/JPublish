98d3faed0c2384c456e3294ed30d196b1128d2e6
wanglei <wanglei@youku.com>
2016-01-18 17:00:46 +0800

    add struct : marathon and base

--- json.go
+++ json.go
@@ -170,3 +170,20 @@ type Group struct {
 	Dependencies []string       `json:"dependencies"`
 	Groups       []*Group       `json:"groups"`
 }
+
+//////////////////////////////////////////////////
+// add struct : Marathon (for marathonctl)
+type Marathon struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+// add struct : base (for docker image)
+type Base struct {
+	BuildPath  string `json:"buildPath"`
+	DeployJson string `json:"deployJson"`
+	Gitlib     string `json:"gitlib"`
+	Registry   string `json:"registry"`
+	DockerPre  string `json:"dockerPre"`
+}
